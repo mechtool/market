9a2c27f657d8aaf7f92b32dda9d4e0cc60bcc030
Denis Romanenko <drom@1c.ru>
2020-08-04 12:34:55 +0300

    BNET-3325 фикс работоспособности приложения в IE11

--- projects/market/src/main.ts
+++ projects/market/src/main.ts
@@ -4,6 +4,17 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import cssVarsPonyfill from 'css-vars-ponyfill';
+
+cssVarsPonyfill({
+  include: 'style',
+  onlyLegacy: false,
+  watch: true,
+  onComplete(cssText, styleNode, cssVariables) {
+    console.log(cssText);
+  }
+});
+
 if (environment.production) {
   enableProdMode();
 }
