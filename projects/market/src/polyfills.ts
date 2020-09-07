e77a48071f4d879dabafb295169c59a9f4ced296
Denis Romanenko <drom@1c.ru>
2020-09-07 12:33:30 +0300

    BNET-3325 Фиксы для IE. Часть#1

--- projects/market/src/polyfills.ts
+++ projects/market/src/polyfills.ts
@@ -43,6 +43,16 @@ import 'classlist.js';  // Run `npm install --save classlist.js`.
 import 'web-animations-js';  // Run `npm install --save web-animations-js`.
 
 /**
+ * Понифил для функционирования CSS Variables / Custom Properties в IE11.
+ */
+import cssVarsPonyfill from 'css-vars-ponyfill';
+cssVarsPonyfill({
+  onlyLegacy: false,
+  watch: true,
+  silent: true,
+});
+
+/**
  * By default, zone.js will patch all possible macroTask and DomEvents
  * user can disable parts of macroTask/DomEvents patch by setting following flags
  * because those flags need to be set before `zone.js` being loaded, and webpack
@@ -65,7 +75,6 @@ import 'web-animations-js';  // Run `npm install --save web-animations-js`.
  *  (window as any).__Zone_enable_cross_context_check = true;
  *
  */
-
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
