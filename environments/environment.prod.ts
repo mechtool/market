819f4cfed2542bf811fb7d7873067e95b6366fa9
Denis Romanenko <drom@1c.ru>
2020-11-23 17:33:53 +0300

    BNET-3854 временно GTMid от дева на прод

--- environments/environment.prod.ts
+++ environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   apiUrl: 'https://api.1cbn.ru/market',
   imgUrl: 'https://api.1cbn.ru',
   itsUrl: 'https://login.1c.ru',
-  gtmID: 'GTM-58T4JKC',
+  gtmID: 'GTM-TW5RXQF',
   production: true,
 };
