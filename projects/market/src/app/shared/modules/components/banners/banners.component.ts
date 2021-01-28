b87cd06fae897adfbc5d60bda3d4cbba26f9a997
Denis Romanenko <romanenko.dennis@gmail.com>
2021-01-28 16:50:48 +0300

    BNET-4038 Фикс обрезания баннера (update)

--- projects/market/src/app/shared/modules/components/banners/banners.component.ts
+++ projects/market/src/app/shared/modules/components/banners/banners.component.ts
@@ -1,22 +1,29 @@
-import { ChangeDetectorRef, Component, Inject, Input, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, Inject, Input, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { DOCUMENT } from '@angular/common';
 import { BannerItemModel } from './models';
-import { combineLatest, fromEvent, interval, Observable, Subject } from 'rxjs';
-import { delay, distinctUntilChanged, take, takeWhile, tap } from 'rxjs/operators';
+import { combineLatest, interval, Observable, Subscription } from 'rxjs';
+import { distinctUntilChanged, take, takeWhile } from 'rxjs/operators';
 import { ResponsiveService } from '#shared/modules/common-services/responsive.service';
 import { NavigationService } from '#shared/modules/common-services/navigation.service';
+import { NzCarouselComponent } from 'ng-zorro-antd/carousel';
+import { unsubscribeList } from '#shared/utils';
 
 @Component({
   selector: 'market-banners',
   templateUrl: './banners.component.html',
   styleUrls: ['./banners.component.scss', './banners.component-768.scss'],
 })
-export class BannersComponent implements OnInit {
+export class BannersComponent implements OnInit, OnDestroy {
   @Input() bannerItems: BannerItemModel[];
   @Input() autoPlay = true;
   bannerItemsGroups: BannerItemModel[][];
   carouselEnabled = false;
   groupDivider: number = null;
+  @ViewChild(NzCarouselComponent) private _carouselRef: NzCarouselComponent;
+  private get _body(): HTMLElement {
+    return this._document.body;
+  }
+  private _carouselVisibilitySubscription: Subscription;
 
   constructor(
     @Inject(DOCUMENT) private _document: Document,
@@ -27,19 +34,24 @@ export class BannersComponent implements OnInit {
 
   ngOnInit() {
     this._handleCarouselVisibility();
-
-    const body = this._document.body;
-    const source$ = interval(30);
-    source$.pipe(
-      takeWhile(() => body.scrollHeight > body.clientHeight),
-      take(1)
-    ).subscribe((res) => {
-      this._reinitChildComponent();
-    })
-
+    this._detectScrollAppearanceAndReinitCarousel();
   }
 
-  private _reinitChildComponent(): void{
+  ngOnDestroy() {
+    unsubscribeList([this._carouselVisibilitySubscription]);
+  }
+
+  private _detectScrollAppearanceAndReinitCarousel() {
+    const source$ = interval(100);
+    const subscription = source$.subscribe(() => {
+      if (this._carouselRef && this._body.scrollHeight > this._body.clientHeight) {
+        subscription.unsubscribe();
+        this._reinitCarousel();
+      }
+    })
+  }
+
+  private _reinitCarousel(): void {
     if (this.carouselEnabled) {
       this.carouselEnabled = false;
       this._cdr.detectChanges();
@@ -49,64 +61,65 @@ export class BannersComponent implements OnInit {
   }
 
   private _handleCarouselVisibility() {
-    combineLatest([this._isNavBarMinifiedChange$(), this._screenWidthChange$()]).subscribe(([isNavBarMinified, screenWidth]) => {
-      const maxItemsInCarouselGroup = 3;
-      let carouselEnabled = false;
-      let groupDivider = null;
+    this._carouselVisibilitySubscription = combineLatest([this._isNavBarMinifiedChange$(), this._screenWidthChange$()])
+      .subscribe(([isNavBarMinified, screenWidth]) => {
+        const maxItemsInCarouselGroup = 3;
+        let carouselEnabled = false;
+        let groupDivider = null;
 
-      if (!isNavBarMinified) {
-        if (screenWidth > 1580 && this.bannerItems.length <= maxItemsInCarouselGroup) {
-          carouselEnabled = false;
+        if (!isNavBarMinified) {
+          if (screenWidth > 1580 && this.bannerItems.length <= maxItemsInCarouselGroup) {
+            carouselEnabled = false;
+          }
+          if (screenWidth > 1580 && this.bannerItems.length > maxItemsInCarouselGroup) {
+            carouselEnabled = true;
+            groupDivider = 3;
+          }
+          if (screenWidth <= 1580 && screenWidth > 768 && this.bannerItems.length < maxItemsInCarouselGroup) {
+            carouselEnabled = false;
+          }
+          if (screenWidth <= 1580 && screenWidth > 768 && this.bannerItems.length >= maxItemsInCarouselGroup) {
+            carouselEnabled = true;
+            groupDivider = 2;
+          }
+          if (screenWidth <= 768 && this.bannerItems.length === 1) {
+            carouselEnabled = false;
+          }
+          if (screenWidth <= 768 && this.bannerItems.length !== 1) {
+            carouselEnabled = true;
+            groupDivider = 1;
+          }
         }
-        if (screenWidth > 1580 && this.bannerItems.length > maxItemsInCarouselGroup) {
-          carouselEnabled = true;
-          groupDivider = 3;
-        }
-        if (screenWidth <= 1580 && screenWidth > 768 && this.bannerItems.length < maxItemsInCarouselGroup) {
-          carouselEnabled = false;
-        }
-        if (screenWidth <= 1580 && screenWidth > 768 && this.bannerItems.length >= maxItemsInCarouselGroup) {
-          carouselEnabled = true;
-          groupDivider = 2;
-        }
-        if (screenWidth <= 768 && this.bannerItems.length === 1) {
-          carouselEnabled = false;
-        }
-        if (screenWidth <= 768 && this.bannerItems.length !== 1) {
-          carouselEnabled = true;
-          groupDivider = 1;
-        }
-      }
 
-      if (isNavBarMinified) {
-        if (screenWidth > 1300 && this.bannerItems.length <= maxItemsInCarouselGroup) {
-          carouselEnabled = false;
+        if (isNavBarMinified) {
+          if (screenWidth > 1300 && this.bannerItems.length <= maxItemsInCarouselGroup) {
+            carouselEnabled = false;
+          }
+          if (screenWidth > 1300 && this.bannerItems.length > maxItemsInCarouselGroup) {
+            carouselEnabled = true;
+            groupDivider = 3;
+          }
+          if (screenWidth <= 1300 && screenWidth > 768 && this.bannerItems.length < maxItemsInCarouselGroup) {
+            carouselEnabled = false;
+          }
+          if (screenWidth <= 1300 && screenWidth > 768 && this.bannerItems.length >= maxItemsInCarouselGroup) {
+            carouselEnabled = true;
+            groupDivider = 2;
+          }
+          if (screenWidth <= 768 && this.bannerItems.length === 1) {
+            carouselEnabled = false;
+          }
+          if (screenWidth <= 768 && this.bannerItems.length !== 1) {
+            carouselEnabled = true;
+            groupDivider = 1;
+          }
         }
-        if (screenWidth > 1300 && this.bannerItems.length > maxItemsInCarouselGroup) {
-          carouselEnabled = true;
-          groupDivider = 3;
-        }
-        if (screenWidth <= 1300 && screenWidth > 768 && this.bannerItems.length < maxItemsInCarouselGroup) {
-          carouselEnabled = false;
-        }
-        if (screenWidth <= 1300 && screenWidth > 768 && this.bannerItems.length >= maxItemsInCarouselGroup) {
-          carouselEnabled = true;
-          groupDivider = 2;
-        }
-        if (screenWidth <= 768 && this.bannerItems.length === 1) {
-          carouselEnabled = false;
-        }
-        if (screenWidth <= 768 && this.bannerItems.length !== 1) {
-          carouselEnabled = true;
-          groupDivider = 1;
-        }
-      }
 
-      this.carouselEnabled = carouselEnabled;
-      if (carouselEnabled && groupDivider) {
-        this.groupDivider = groupDivider;
-        this._resetBannerItemsGroupsBy(groupDivider);
-      }
+        this.carouselEnabled = carouselEnabled;
+        if (carouselEnabled && groupDivider) {
+          this.groupDivider = groupDivider;
+          this._resetBannerItemsGroupsBy(groupDivider);
+        }
     });
   }
 
