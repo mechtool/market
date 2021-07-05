b4a18321f87507c221c9792c835b3998ed620bdd
Sitnikov_E <esit@1c.ru>
2021-07-05 13:28:49 +0300

    исправление замечаний

--- projects/market/src/app/shared/modules/common-services/sw-update.service.ts
+++ projects/market/src/app/shared/modules/common-services/sw-update.service.ts
@@ -11,10 +11,7 @@ export class SwUpdateService implements OnDestroy{
 
     if (isPlatformBrowser(this._platformId) && this.swUpdate.isEnabled){
         this.subs.push(this.swUpdate.available.subscribe( () => {
-          // todo Временная функция подтверждения
-          if (this.confirmUser()) {
-            this.swUpdate.activateUpdate().then(() => document.location.reload());
-          }
+            this.swUpdate.activateUpdate();
         }));
     }
   }
@@ -23,7 +20,4 @@ export class SwUpdateService implements OnDestroy{
     this.subs.forEach( (s) => s.unsubscribe());
   }
 
-  confirmUser(){
-   return window.confirm('Загрузить обновленное приложение?')
-  }
 }
