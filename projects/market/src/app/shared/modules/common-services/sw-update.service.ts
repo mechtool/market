c036f82164848e66befb9405e7bcce7c920b6a42
Sitnikov_E <esit@1c.ru>
2021-07-26 11:37:17 +0300

    BNE-4524 Check-cache

--- projects/market/src/app/shared/modules/common-services/sw-update.service.ts
+++ projects/market/src/app/shared/modules/common-services/sw-update.service.ts
@@ -1,15 +1,16 @@
 import {Inject, Injectable, OnDestroy, PLATFORM_ID} from '@angular/core';
-import { isPlatformBrowser} from '@angular/common';
+import {DOCUMENT, isPlatformBrowser} from '@angular/common';
 import {SwUpdate} from '@angular/service-worker';
 
 @Injectable()
 export class SwUpdateService implements OnDestroy {
+  private caches;
   private subs = [];
 
   constructor(
+    @Inject(DOCUMENT) private doc : Document,
     @Inject(PLATFORM_ID) private _platformId: Object,
-    private swUpdate: SwUpdate) {
-  }
+    private swUpdate: SwUpdate) {}
 
   ngOnDestroy() {
     this.subs.forEach((s) => s.unsubscribe());
@@ -17,8 +18,11 @@ export class SwUpdateService implements OnDestroy {
 
   setUp() {
     if (isPlatformBrowser(this._platformId) && this.swUpdate.isEnabled) {
+      this.caches = this.doc?.defaultView?.caches;
       this.subs.push(this.swUpdate.available.subscribe(() => {
-        this.swUpdate.activateUpdate();
+        this._deleteCaches().then(()=> {
+            this.swUpdate.activateUpdate();
+        })
       }));
       const t = setTimeout(() => {
         this.swUpdate.checkForUpdate();
@@ -26,4 +30,14 @@ export class SwUpdateService implements OnDestroy {
       }, 500)
     }
   }
+  async _deleteCaches(){
+    const cacheNames = await this.caches.keys();
+    return new Promise((res)=> {
+      cacheNames.forEach((cacheName) => {
+        this.caches.delete(cacheName)
+      })
+      res(true);
+    });
+  }
 }
+
--- projects/market/src/market.ngsw.js
+++ projects/market/src/market.ngsw.js
@@ -2,7 +2,7 @@
 importScripts('ngsw-worker.js');
 let version = '0.0.1';
 let channel = new BroadcastChannel('sw-messages');
-channel.addEventListener('message', (event)=> {
+channel.addEventListener('message', async (event)=> {
   if (event.data.type === 'version') {
     channel.postMessage({type : 'version', version : version});
   }
