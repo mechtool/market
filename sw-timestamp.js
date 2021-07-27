a902dbbd7ae929dff6d9e2c04849b23dc1be53ed
Sitnikov_E <esit@1c.ru>
2021-07-27 18:41:48 +0300

    BNE-4524 caches-13

--- sw-timestamp.js
+++ sw-timestamp.js
@@ -3,9 +3,3 @@ try {
   let str = fs.readFileSync('./dist/market/stage/market.ngsw.js');
   fs.writeFileSync('./dist/market/stage/market.ngsw.js', `${str}; let stamp = ${ Math.random()};`)
 }catch (err){true};
-try {
-  let str = fs.readFileSync('./dist/market/prod/market.ngsw.js');
-  fs.writeFileSync('./dist/market/prod/market.ngsw.js', `${str}; let stamp = ${ Math.random()};`)
-}catch (err){true}
-
-
